bfeafb0231fa2979c34ed2f117af8455f795e2a0
john.paulett <john.paulett@cb573595-e353-0410-b2f2-bf1a872c5f99>
2008-08-08 19:23:14 +0000

    Downgrade properties to allow older python to work.

--- trunk/stopwatch/stopwatch/__init__.py
+++ trunk/stopwatch/stopwatch/__init__.py
@@ -8,7 +8,8 @@
 
 import time
 
-"""Simple timer.  Great for finding out how long code takes to execute.
+"""stopwatch is a very simple python module for measuring time.
+Great for finding out how long code takes to execute.
 
 >>> import stopwatch
 >>> t = stopwatch.Timer()
@@ -21,26 +22,57 @@ import time
 >>> print t
 """
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
+__author__ = 'John Paulett <http://blog.7oars.com>'
 
 class Timer(object):
     def __init__(self):
         self.__stopped = None
-        self.__start = time.time()
+        self.__start = self.__time()
   
     def stop(self):
-        self.__stopped = self.__time()
+        """Stops the clock permanently for the instance of the Timer.
+        Returns the time at which the instance was stopped.
+        """
+        self.__stopped = self.__last_time()
         return self.elapsed
 
-    @property
     def elapsed(self):
-        return self.__time() - self.__start
-        
-    def __time(self):
+        """The number of seconds since the current time that the Timer
+        object was created.  If stop() was called, it is the number
+        of seconds from the instance creation until stop() was called.
+        """
+        return self.__last_time() - self.__start
+    elapsed = property(elapsed)
+    
+    def start_time(self):
+        """The time at which the Timer instance was created.
+        """
+        return self.__start
+    start_time = property(start_time)
+    
+    def stop_time(self):
+        """The time at which stop() was called, or None if stop was 
+        never called.
+        """
+        return self.__stopped 
+    stop_time = property(stop_time)        
+    
+    def __last_time(self):
+        """Return the current time or the time at which stop() was call,
+        if called at all.
+        """
         if self.__stopped is not None:
             return self.__stopped
+        return self.__time()
+    
+    def __time(self):
+        """Wrapper for time.time() to allow unit testing.
+        """
         return time.time()
     
     def __str__(self):
+        """Nicely format the elapsed time
+        """
         return str(self.elapsed) + ' sec'
     
